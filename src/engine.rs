20302a6126bb0d06fb8211838586aa22bbf91e6b
Chay Nabors <chaynabors@gmail.com>
2024-01-28 17:58:15 -0600

    add a few helper types

--- src/engine.rs
+++ src/engine.rs
@@ -1,31 +1,67 @@
-use anyhow::Result;
-use winit::{event::{Event, WindowEvent}, event_loop::EventLoop, window::{Window, WindowBuilder}};
+use std::sync::Arc;
 
-pub struct Engine {
+use anyhow::Result;
+use wgpu::{Color, LoadOp, Operations, RenderPassColorAttachment, RenderPassDescriptor, StoreOp};
+use winit::{dpi::LogicalSize, event::{Event, WindowEvent}, event_loop::EventLoop, window::{Window, WindowBuilder}};
+
+use crate::graphics_context::GraphicsContext;
+
+const WINDOW_WIDTH: u32 = 1280;
+const WINDOW_HEIGHT: u32 = 720;
+
+pub struct Engine<'a> {
     event_loop: EventLoop<()>,
-    window: Window,
+    window: Arc<Window>,
+    gfx: GraphicsContext<'a>,
 }
 
-impl Engine {
+impl<'a> Engine<'a> {
     pub fn new() -> Result<Self> {
         let event_loop = EventLoop::new()?;
-        let window = WindowBuilder::new().build(&event_loop)?;
+        let window_size = LogicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT);
+        let window = Arc::new(WindowBuilder::new().with_inner_size(window_size).build(&event_loop)?);
+        let gfx = GraphicsContext::new(window.clone(), WINDOW_WIDTH, WINDOW_HEIGHT)?;
 
         Ok(Self {
             event_loop,
             window,
+            gfx,
         })
     }
 
-    pub fn run(self) -> Result<()> {
+    pub fn run(mut self) -> Result<()> {
         self.event_loop.run(|event, elwt| {
             match event {
                 Event::WindowEvent { window_id, event } => if window_id == self.window.id() {
                     match event {
-                        WindowEvent::Resized(_) => (),
+                        WindowEvent::Resized(new_size) => self.gfx.resize(new_size.width, new_size.height),
                         WindowEvent::CloseRequested => elwt.exit(),
                         WindowEvent::Destroyed => (),
-                        WindowEvent::RedrawRequested => (),
+                        WindowEvent::RedrawRequested => {
+                            if let Some((surface, view)) = self.gfx.get_frame() {
+                                let mut encoder = self.gfx.create_command_encoder("primary");
+
+                                encoder.begin_render_pass(&RenderPassDescriptor {
+                                    label: None,
+                                    color_attachments: &[Some(RenderPassColorAttachment {
+                                        view: &view,
+                                        resolve_target: None,
+                                        ops: Operations {
+                                            load: LoadOp::Clear(Color::BLACK),
+                                            store: StoreOp::Store,
+                                        },
+                                    })],
+                                    depth_stencil_attachment: None,
+                                    timestamp_writes: None,
+                                    occlusion_query_set: None,
+                                });
+                                
+                                let commands = encoder.finish();
+                                
+                                self.gfx.submit([commands]);
+                                surface.present();
+                            }
+                        },
                         _ => (),
                     }
                 },
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #![windows_subsystem = "windows"]
 
 mod engine;
+mod asset;
+mod graphics_context;
+mod state;
 
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 
 #[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
     command: Option<Command>,
@@ -18,6 +22,18 @@ enum Command {
 }
 
 fn main() -> Result<()> {
+    #[cfg(windows)]
+    unsafe {
+        use windows::Win32::System::Console::{
+            AttachConsole,
+            ATTACH_PARENT_PROCESS
+        };
+
+        AttachConsole(ATTACH_PARENT_PROCESS).ok();
+    }
+
+    tracing_subscriber::fmt::init();
+
     match Cli::parse().command.unwrap_or(Command::Launch) {
         Command::Launch => engine::Engine::new()?.run()?,
         Command::Update => {
